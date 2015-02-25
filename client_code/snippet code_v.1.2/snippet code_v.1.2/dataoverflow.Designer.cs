0ba2e02a0af66061e0265ac58ebbde5b8c79c963
SeraphLucifer <prince_seraph@hotmail.com>
2015-02-25 11:13:44 +0700

    client ver2

---
+++ client_code/snippet code_v.1.2/snippet code_v.1.2/dataoverflow.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace snippet_code_v._1._2
+{
+    partial class dataoverflow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jAVAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pYTHONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTACKOVERFLOWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wINDOWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jAVAToolStripMenuItem,
+            this.cToolStripMenuItem,
+            this.vBToolStripMenuItem,
+            this.pYTHONToolStripMenuItem,
+            this.sTACKOVERFLOWToolStripMenuItem,
+            this.wINDOWToolStripMenuItem,
+            this.hELPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(903, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jAVAToolStripMenuItem
+            // 
+            this.jAVAToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.jAVAToolStripMenuItem.Name = "jAVAToolStripMenuItem";
+            this.jAVAToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.jAVAToolStripMenuItem.Text = "JAVA";
+            this.jAVAToolStripMenuItem.Click += new System.EventHandler(this.jAVAToolStripMenuItem_Click);
+            // 
+            // cToolStripMenuItem
+            // 
+            this.cToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.cToolStripMenuItem.Name = "cToolStripMenuItem";
+            this.cToolStripMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.cToolStripMenuItem.Text = "C#";
+            // 
+            // vBToolStripMenuItem
+            // 
+            this.vBToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.vBToolStripMenuItem.Name = "vBToolStripMenuItem";
+            this.vBToolStripMenuItem.Size = new System.Drawing.Size(33, 20);
+            this.vBToolStripMenuItem.Text = "VB";
+            // 
+            // pYTHONToolStripMenuItem
+            // 
+            this.pYTHONToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.pYTHONToolStripMenuItem.Name = "pYTHONToolStripMenuItem";
+            this.pYTHONToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.pYTHONToolStripMenuItem.Text = "PYTHON";
+            // 
+            // sTACKOVERFLOWToolStripMenuItem
+            // 
+            this.sTACKOVERFLOWToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.sTACKOVERFLOWToolStripMenuItem.Name = "sTACKOVERFLOWToolStripMenuItem";
+            this.sTACKOVERFLOWToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.sTACKOVERFLOWToolStripMenuItem.Text = "STACKOVERFLOW";
+            this.sTACKOVERFLOWToolStripMenuItem.Click += new System.EventHandler(this.sTACKOVERFLOWToolStripMenuItem_Click);
+            // 
+            // wINDOWToolStripMenuItem
+            // 
+            this.wINDOWToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateToolStripMenuItem});
+            this.wINDOWToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.wINDOWToolStripMenuItem.Name = "wINDOWToolStripMenuItem";
+            this.wINDOWToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.wINDOWToolStripMenuItem.Text = "WINDOW";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.updateToolStripMenuItem.Text = "Update";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewHelpToolStripMenuItem});
+            this.hELPToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hELPToolStripMenuItem.Text = "HELP";
+            // 
+            // viewHelpToolStripMenuItem
+            // 
+            this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.viewHelpToolStripMenuItem.Text = "View Help";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::snippet_code_v._1._2.Properties.Resources._2_2_2558_22_43_38;
+            this.pictureBox1.Location = new System.Drawing.Point(88, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(737, 437);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(750, 489);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go to Page";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataoverflow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(903, 524);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "dataoverflow";
+            this.Text = "Stackoverflow";
+            this.Load += new System.EventHandler(this.dataoverflow_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jAVAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pYTHONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wINDOWToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTACKOVERFLOWToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
