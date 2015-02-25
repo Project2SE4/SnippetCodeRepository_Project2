7b215f5dfc77e0452c0f3b23e7689f0293547098
Arachat  Chokchai <arachat.69@gmail.com>
2015-02-25 10:27:14 +0700

    CodeDB
    SnippetDB V1.5

---
+++ Snippet_CodeDB/SnippetConnect/SnippetConnect/HomeForm.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/HomeSite.Master" AutoEventWireup="true" CodeBehind="HomeForm.aspx.cs" Inherits="SnippetConnect.HomeForm" %>
---
+++ Snippet_CodeDB/SnippetConnect/SnippetConnect/Logout.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SnippetConnect
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Request.Cookies["admin"]!=null)
+            {
+                HttpCookie c = new HttpCookie("admin");
+
+                c.Expires = DateTime.Now.AddYears(-1);
+                Response.Cookies.Add(c);
+            }
+            Response.Redirect("HomeForm.aspx");
+        }
+    }
+}
