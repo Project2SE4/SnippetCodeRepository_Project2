b1ab50a35b86dfe25d39b159a1ea692e30994042
Seraph Lucifer <prince_seraph@hotmail.com>
2015-03-06 08:54:43 +0700

    client 1.4

---
+++ client_code/snippet code_v.1.4/snippet code_v.1.2/dataoverflow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snippet_code_v._1._2
+{
+    public partial class dataoverflow : Form
+    {
+        public dataoverflow()
+        {
+            InitializeComponent();
+        }
+
+        private void dataoverflow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void jAVAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            snippet_code snippetcodeForm = new snippet_code();
+            snippetcodeForm.Show();
+
+            this.Hide();
+        }
+
+        private void sTACKOVERFLOWToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stackoverflow stackoverflowForm = new stackoverflow();
+            stackoverflowForm.Show();
+
+            this.Hide();
+            //this.Close();
+        }
+
+        private void uPDATEToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            update_program updateForm = new update_program();
+            updateForm.Show();
+        }
+    }
+}
