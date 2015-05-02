020387bedab10d501cd30af74548f78695ee74e7
Seraph <prince_seraph@hotmail.com>
2015-05-02 09:15:03 +0700

    Client 2.2-2.3 demo

---
+++ client_code/snippet code_v.2.3 Demo/snippet code_v.1.2/StackoverflowForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace snippet_code_v._1._2
+{
+    partial class StackoverflowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jAVAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pYTHONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTACKOVERFLOWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uPDATEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jAVAToolStripMenuItem,
+            this.cToolStripMenuItem,
+            this.vBToolStripMenuItem,
+            this.pYTHONToolStripMenuItem,
+            this.sTACKOVERFLOWToolStripMenuItem,
+            this.uPDATEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(803, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jAVAToolStripMenuItem
+            // 
+            this.jAVAToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.jAVAToolStripMenuItem.Name = "jAVAToolStripMenuItem";
+            this.jAVAToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.jAVAToolStripMenuItem.Text = "JAVA";
+            this.jAVAToolStripMenuItem.Click += new System.EventHandler(this.jAVAToolStripMenuItem_Click);
+            // 
+            // cToolStripMenuItem
+            // 
+            this.cToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.cToolStripMenuItem.Name = "cToolStripMenuItem";
+            this.cToolStripMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.cToolStripMenuItem.Text = "C#";
+            this.cToolStripMenuItem.Click += new System.EventHandler(this.cToolStripMenuItem_Click);
+            // 
+            // vBToolStripMenuItem
+            // 
+            this.vBToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.vBToolStripMenuItem.Name = "vBToolStripMenuItem";
+            this.vBToolStripMenuItem.Size = new System.Drawing.Size(33, 20);
+            this.vBToolStripMenuItem.Text = "VB";
+            this.vBToolStripMenuItem.Click += new System.EventHandler(this.vBToolStripMenuItem_Click);
+            // 
+            // pYTHONToolStripMenuItem
+            // 
+            this.pYTHONToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.pYTHONToolStripMenuItem.Name = "pYTHONToolStripMenuItem";
+            this.pYTHONToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.pYTHONToolStripMenuItem.Text = "PYTHON";
+            this.pYTHONToolStripMenuItem.Click += new System.EventHandler(this.pYTHONToolStripMenuItem_Click);
+            // 
+            // sTACKOVERFLOWToolStripMenuItem
+            // 
+            this.sTACKOVERFLOWToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.sTACKOVERFLOWToolStripMenuItem.Name = "sTACKOVERFLOWToolStripMenuItem";
+            this.sTACKOVERFLOWToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.sTACKOVERFLOWToolStripMenuItem.Text = "STACKOVERFLOW";
+            // 
+            // uPDATEToolStripMenuItem
+            // 
+            this.uPDATEToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.uPDATEToolStripMenuItem.Name = "uPDATEToolStripMenuItem";
+            this.uPDATEToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.uPDATEToolStripMenuItem.Text = "UPDATE";
+            this.uPDATEToolStripMenuItem.Click += new System.EventHandler(this.uPDATEToolStripMenuItem_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(372, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(297, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Question";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(130, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Accepted";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(596, 105);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(38, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "TRUE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::snippet_code_v._1._2.Properties.Resources.se_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(134, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(580, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 137);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(803, 445);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // StackoverflowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(803, 583);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "StackoverflowForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snippet Code Demo";
+            this.Load += new System.EventHandler(this.stackoverflow_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jAVAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pYTHONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTACKOVERFLOWToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uPDATEToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ client_code/snippet code_v.2.3 Demo/snippet code_v.1.2/update_program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace snippet_code_v._1._2
+{
+    public partial class update_program : Form
+    {
+        public update_program()
+        {
+            InitializeComponent();
+        }
+
+        private string source_file = "";
+        private string destination_file = "";
+
+        private void update_program_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] args = Environment.GetCommandLineArgs();
+                source_file = args[1].Replace("@", " ");
+                destination_file = args[2].Replace("@", " ");
+                System.IO.File.Copy(source_file, destination_file, true);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(destination_file))
+            {
+                Process.Start(destination_file);
+            }
+            this.Hide();
+            //Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(destination_file))
+            {
+                Process.Start(destination_file);
+            }
+            this.Hide();
+            //Application.Exit();
+        }
+
+
+        
+    }
+}
